2ee6a30d72bc8b4a0b0abbf15cd586dd291dff8b
pdurkin84 <root@gpfs2.pdurkin.mids>
2018-05-31 07:59:27 -0500

    Updates for subdir

--- HW4/process_mumbler_files.sh
+++ HW4/process_mumbler_files.sh
@@ -9,12 +9,17 @@ host_name=$(hostname|cut -d"." -f1)
 # if we have a command line argument it should be a subdirectory to process
 if [[ $# -eq 1 && $1 =~ [[:digit:]] ]]
 then
+	if [[ ! -d $STORAGE_DIR/$host_name/$1/ ]]
+	then
+		echo "Error cannot find subdir $1 under $STORAGE_DIR/$host_name/"
+		exit 1
+	fi
 	SOURCE_FILE_DIR=$STORAGE_DIR/$host_name/$1/
 else
 	SOURCE_FILE_DIR=$STORAGE_DIR/$host_name/
 fi
 
-OUTPUT_FILE=$STORAGE_DIR/$host_name/processed
+OUTPUT_FILE=$SOURCE_FILE_DIR/processed
 STOP_INDEX=0
 MAX_LINES=1000
 FILE_NUMBER=0
@@ -33,6 +38,8 @@ do
 	CURRENT_KEY_COUNT=0
 	unzip -p $file | while IFS=$'\t' read -r -a myInputArray
 	do
+		# only taking keys that start with an alphabetical character, the
+		# script is for mumbles which means no numbers or punctuations
 		if [[ ${myInputArray[0]:0:1} =~ [[:alpha:]] ]]
 		then
 			if [[ ${myInputArray[0]} == "$LAST_KEY" ]]
